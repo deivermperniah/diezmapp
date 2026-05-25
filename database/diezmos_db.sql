ebd35b60cd47c0bd7381d753898f9c4d472818b9
Deiver Pernia <deivermph@gmail.com>
2026-05-24 21:03:02 -0500

    create initial database schema for diezmos_db with tables for iglesia, moneda, miembro, sobre, ofrenda_colaboracion, and transferencia

---
+++ database/diezmos_db.sql
@@ -0,0 +1,73 @@
+-- Base de Datos: Diezmos y Ofrendas
+-- Modelo normalizado hasta 3FN
+-- PostgreSQL
+
+CREATE DATABASE diezmos_db;
+
+-- \c diezmos_db
+
+CREATE TABLE IGLESIA (
+    Id_Iglesia SERIAL PRIMARY KEY,
+    Nombre_Iglesia VARCHAR(100) NOT NULL,
+    Ciudad VARCHAR(80) NOT NULL
+);
+
+CREATE TABLE MONEDA (
+    Id_Moneda SERIAL PRIMARY KEY,
+    Nombre_Moneda VARCHAR(50) NOT NULL,
+    Simbolo VARCHAR(10) NOT NULL
+);
+
+CREATE TABLE MIEMBRO (
+    Id_Miembro SERIAL PRIMARY KEY,
+    Nombre VARCHAR(100) NOT NULL,
+    Email VARCHAR(100) UNIQUE,
+    Id_Iglesia INTEGER NOT NULL,
+    FOREIGN KEY (Id_Iglesia)
+    REFERENCES IGLESIA(Id_Iglesia)
+);
+
+CREATE TABLE SOBRE (
+    Numero_Sobre SERIAL PRIMARY KEY,
+    Fecha DATE NOT NULL,
+    Id_Miembro INTEGER NOT NULL,
+    Monto_Diezmo DECIMAL(10,2) NOT NULL,
+    Id_Moneda_Diezmo INTEGER,
+    Monto_Pacto_Amor DECIMAL(10,2),
+    Id_Moneda_Pacto INTEGER,
+    Total_Incluido DECIMAL(10,2) NOT NULL,
+
+    FOREIGN KEY (Id_Miembro)
+    REFERENCES MIEMBRO(Id_Miembro),
+
+    FOREIGN KEY (Id_Moneda_Diezmo)
+    REFERENCES MONEDA(Id_Moneda),
+
+    FOREIGN KEY (Id_Moneda_Pacto)
+    REFERENCES MONEDA(Id_Moneda)
+);
+
+CREATE TABLE OFRENDA_COLABORACION (
+    Id_Ofrenda SERIAL PRIMARY KEY,
+    Numero_Sobre INTEGER NOT NULL,
+    Monto_Ofrenda DECIMAL(10,2) NOT NULL,
+    Id_Moneda INTEGER NOT NULL,
+
+    FOREIGN KEY (Numero_Sobre)
+    REFERENCES SOBRE(Numero_Sobre),
+
+    FOREIGN KEY (Id_Moneda)
+    REFERENCES MONEDA(Id_Moneda)
+);
+
+CREATE TABLE TRANSFERENCIA (
+    Id_Transferencia SERIAL PRIMARY KEY,
+    Numero_Sobre INTEGER NOT NULL,
+    Fecha_Transferencia DATE NOT NULL,
+    Numero_Transferencia VARCHAR(50) NOT NULL,
+    Banco_Receptor_Cuenta VARCHAR(120) NOT NULL,
+    Monto_Transferencia DECIMAL(10,2) NOT NULL,
+
+    FOREIGN KEY (Numero_Sobre)
+    REFERENCES SOBRE(Numero_Sobre)
+);
